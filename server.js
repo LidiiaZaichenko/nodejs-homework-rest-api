5f22da7a6e0bb0ebe6342ae92ea5fb568831228b
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2024-01-17 15:54:40 +0200

    add mongodb

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import app from "./app.js";
 
 const DB_HOST =
-  "mongodb+srv://Lzaichenko:<7bPN1yKX5W5XLShm>@cluster0.sjyuo1b.mongodb.net/my-contacts?retryWrites=true&w=majority";
-mongoose
-  .connect(DB_HOST)
+  "mongodb+srv://Lzaichenko:7bPN1yKX5W5XLShm@cluster0.sjyuo1b.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+  mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
       console.log("Server running. Use our API on port: 3000");
