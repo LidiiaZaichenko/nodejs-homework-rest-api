2acf3b72a7b03e6879c0413081770500fc887009
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2024-01-18 17:07:50 +0200

    add decorators

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,101 +1,60 @@
 import Contact from "../models/Contact.js";
 import { HttpError } from "../helpers/index.js";
-import {
-  contactAddSchema,
-  contactUpdateSchema,
-  contactUpdateFavoriteSchema,
-} from "../models/Contact.js";
+import { ctrlWrapper } from "../decorators/index.js";
 
-const getAll = async (req, res, next) => {
-  try {
-    const result = await Contact.find({}, "-createdAt -updatedAt");
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getAll = async (req, res) => {
+  const result = await Contact.find({}, "-createdAt -updatedAt");
+  res.json(result);
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Contact.findById(id);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+  if (!result) {
+    throw HttpError(404, `Movie with id=${id} not found`);
   }
+  res.json(result);
 };
 
-const add = async (req, res, next) => {
-  try {
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await Contact.create(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+const add = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
 };
 
-const updateIdContact = async (req, res, next) => {
-  try {
-    const { error } = contactUpdateSchema.validate(req.body);
-    if (error) {
-        throw HttpError(400, error.message);
-    }
-    const {id} = req.params;
-    const result = await Contact.findByIdAndUpdate(id,req.body);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateIdContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+  if (!result) {
+    throw HttpError(404, `Movie with id=${id} not found`);
   }
+  res.json(result);
 };
 
-const deliteById = async (req, res, next) => {
-  try {
-    const {id} = req.params;
-    const result = await Contact.findByIdAndDelete(id);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${id} not found`);
-    }
-    res.json({
-      message: "contact deleted"
-  })
-
-  } catch (error) {
-    next(error);
+const deliteById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404, `Movie with id=${id} not found`);
   }
+  res.json({
+    message: "contact deleted",
+  });
 };
 
-const updateFavoriteContact = async (req, res, next) => {
-  try {
-    const { error } = contactUpdateFavoriteSchema.validate(req.body);
-    if (error) {
-        throw HttpError(400, error.message);
-    }
-    const {id} = req.params;
-    const result = await Contact.findByIdAndUpdate(id,req.body);
-    if (!result) {
-      throw HttpError(404, `Movie with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateFavoriteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+  if (!result) {
+    throw HttpError(404, `Movie with id=${id} not found`);
   }
+  res.json(result);
 };
 
 export default {
-  getAll,
-  getById,
-  add,
-  updateIdContact,
-  updateFavoriteContact,
-  deliteById,
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateIdContact: ctrlWrapper(updateIdContact),
+  updateFavoriteContact: ctrlWrapper(updateFavoriteContact),
+  deliteById: ctrlWrapper(deliteById),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,18 +4,43 @@ import contactsController from "../../controllers/contacts-controller.js";
 
 import { isEmptyBody, isValidId } from "../../middlewares/index.js";
 
+import { validateBody } from "../../decorators/index.js";
+
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+  contactUpdateFavoriteSchema,
+} from "../../models/Contact.js";
+
 const router = express.Router();
 
 router.get("/", contactsController.getAll);
 
 router.get("/:id", isValidId, contactsController.getById);
 
-router.post("/", isEmptyBody, contactsController.add);
+router.post(
+  "/",
+  isEmptyBody,
+  validateBody(contactAddSchema),
+  contactsController.add
+);
 
-router.delete("/:id",isValidId, contactsController.deliteById);
+router.delete("/:id", isValidId, contactsController.deliteById);
 
-router.put("/:id",isValidId, isEmptyBody, contactsController.updateIdContact);
+router.put(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactUpdateSchema),
+  contactsController.updateIdContact
+);
 
-router.patch("/:id/favorite", isValidId, isEmptyBody, contactsController.updateFavoriteContact);
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactUpdateFavoriteSchema),
+  contactsController.updateFavoriteContact
+);
 
 export default router;
