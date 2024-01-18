423aa982a177284b59698f7f4d693a4dd7f3d220
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2024-01-18 20:31:28 +0200

    add token

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,16 +1,23 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import { HttpError } from "../helpers/index.js";
 
 import { ctrlWrapper } from "../decorators/index.js";
 
+const {JWT_SECRET} = process.env;
 
 const signup = async(req, res)=> {
-    const {email} = req.body;
+    const {email, password} = req.body;
     const user = await User.findOne({email});
     if(user) {
         throw HttpError(409, "Email already in use");
     }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({...req.body, password: hashPassword});
 
     res.json({
         username: newUser.username,
@@ -19,9 +26,28 @@ const signup = async(req, res)=> {
 }
 
 const signin = async(req, res)=> {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if(!user) {
+        throw HttpError(401, "Email or password invalid");
+    }
 
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare) {
+        throw HttpError(401, "Email or password invalid");
+    }
+
+    const {_id: id} = user;
+    const payload = {
+        id
+    };
+
+    const token =jwt.sign(payload, JWT_SECRET, {expiresIn: "23h"});
+
+    res.json({
+        token,
+    })
 }
-
 
 export default {
     signup: ctrlWrapper(signup),
